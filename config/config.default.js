b3616da2ac30189c290e2e6bf07ae9cd2f306b4c
cxy <chenxiaoyi27@163.com>
2023-09-14 16:11:00 +0800

    连接阿里云rds数据库

--- config/config.default.js
+++ config/config.default.js
@@ -46,8 +46,8 @@ module.exports = appInfo => {
 
   config.sequelize = {
     dialect: 'mysql',
-    host: '114.55.85.81', // 本地docker启动：mysql；本地开发：127.0.0.1
-    port: 3307, // 本地docker启动：3306；本地开发：3306
+    host: 'rm-bp1g9178k3ax0399l9o.mysql.rds.aliyuncs.com', // (docker)tianchi-mysql: '114.55.85.81', // 本地docker启动：mysql；本地开发：127.0.0.1
+    port: 3306, // docker:3307; 本地docker启动：3306；本地开发：3306
     password: '123456',
     database: 'lowcode',
   };
